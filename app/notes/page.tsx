8e0045f38073d7187251894cf5b394579ab96203
kiqueangulo <kiqueangulo@gmail.com>
2022-11-02 02:35:10 -0400

    Create Note component

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link"
+
 async function getNotes() {
   const res = await fetch(
     "http://127.0.0.1:8090/api/collections/notes/records?page=1&perPage=30"
@@ -20,5 +22,19 @@ export default async function NotesPage() {
         ))}
       </div>
     </div>
+  )
+}
+
+function Note({ note }: any) {
+  const { id, title, content, created } = note || {}
+
+  return (
+    <Link href={`/notes/${id}`}>
+      <div>
+        <h2>{title}</h2>
+        <h5>{content}</h5>
+        <p>{created}</p>
+      </div>
+    </Link>
   )
 }
