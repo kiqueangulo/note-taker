18aae1f86f2c6724653dbeb5bc7c6c6eef0f7022
kiqueangulo <kiqueangulo@gmail.com>
2022-11-02 02:30:28 -0400

    Fetch notes to use in Note component

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,7 +1,24 @@
+async function getNotes() {
+  const res = await fetch(
+    "http://127.0.0.1:8090/api/collections/notes/records?page=1&perPage=30"
+  )
+  const data = await res.json()
+
+  return data?.items as any[]
+}
+
 export default async function NotesPage() {
+  const notes = await getNotes()
+
   return (
     <div>
       <h1>Notes</h1>
+
+      <div>
+        {notes?.map(note => (
+          <Note key={note.id} note={note} />
+        ))}
+      </div>
     </div>
   )
 }
