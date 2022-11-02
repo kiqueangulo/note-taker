38cb5206929950802f41b50a688e9cb838f14156
kiqueangulo <kiqueangulo@gmail.com>
2022-11-02 02:48:58 -0400

    Do not cache data so it refetch on every request

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -2,7 +2,8 @@ import Link from "next/link"
 
 async function getNotes() {
   const res = await fetch(
-    "http://127.0.0.1:8090/api/collections/notes/records?page=1&perPage=30"
+    "http://127.0.0.1:8090/api/collections/notes/records?page=1&perPage=30",
+    { cache: "no-store" }
   )
   const data = await res.json()
 
